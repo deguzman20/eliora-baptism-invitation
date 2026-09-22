b4f002a8afaee640045bce70f1cb32ed4c281045
deguzman20 <alejandrogacumadeguzman@gmail.com>
2026-09-22 23:37:34 +0800

    configure firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyD60lXbSJKTBxp25w98lvN5hVfrDvqwUP0",
+  authDomain: "eliora-faye-christining.firebaseapp.com",
+  projectId: "eliora-faye-christining",
+  storageBucket: "eliora-faye-christining.firebasestorage.app",
+  messagingSenderId: "290667839844",
+  appId: "1:290667839844:web:8f30f21fbab02ff95f7a71",
+  measurementId: "G-WR6H3J2QMV",
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
